0cfd70a5215f2172f168ff8e8bdae28c6087a530
RodionHordiienko <rodyaforbusines@gmail.com>
2023-02-19 00:30:28 +0200

    Init app

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app dark>
+    <NuxtLink to="/"> Home page </NuxtLink>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12"> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home Page',
+  },
+  data: () => ({}),
+}
+</script>
